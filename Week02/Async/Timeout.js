9e1720a46728bfaafb2775a7dd30a19c3dd9f940
utkarsh240 <93428291+utkarsh240@users.noreply.github.com>
2025-01-09 01:32:14 +0530

    .

---
+++ Week02/Async/Timeout.js
@@ -0,0 +1,16 @@
+console.log("Hey");
+
+function timeout(){
+    console.log("Timer")
+}
+
+setTimeout(timeout, 5000);
+
+console.log("Welcome");
+
+let c =0;
+for(let i = 0 ;i<100000; i++){
+    c =c+1;
+}
+
+console.log("Skip");
