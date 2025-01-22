66a553a200840ae7b0f5db93418b2c77390417cd
utkarsh240 <93428291+utkarsh240@users.noreply.github.com>
2025-01-22 23:45:39 +0530

    .

--- Week04/ToDoApp/index2.js
+++ Week04/ToDoApp/index2.js
@@ -3,12 +3,34 @@ const app = express();
 
 // function that return a boolean
 
-function is
 
-app.get("/ride1 ", function(req,res){
-    res.json({
-        msg: "Hello Utkarsh"
-    })
-})
+function isOldEnoughMiddleware(req,res,next){
+    const age = req.query.age;
+    if(age>=14){
+        next();
+    }else{
+        res.json({
+            msg: " Sorry for your age",
+        })
+    }
+}
 
-app.listen(3000);
+app.get("/ride2", isOldEnoughMiddleware, function(req,res){
+        res.json({
+            msg: "success for R2",
+        });
+    
+    });
+
+
+app.get("/ride1", isOldEnoughMiddleware, function(req,res){
+        res.json({
+            msg: "success for R1",
+        });
+    
+});
+
+
+
+
+app.listen(3001);
