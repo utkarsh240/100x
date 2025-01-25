7c7af7e99c8165d30ae18dea16701ecb737fff86
utkarsh240 <93428291+utkarsh240@users.noreply.github.com>
2025-01-26 01:03:01 +0530

    .

---
+++ Week05/Http_Server/Middleware.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+
+let requestcount =0;
+
+function requestInc(req,res,next){
+    requestcount = requestcount+1;
+    console.log(requestcount);
+    next();
+}
+
+function SumHAndler(req,res){
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        ans: a+b;
+    })
+}
+
+
+
+app.get("/sum",function(requestInc,SumHAndler));
+
+app.listen(3000);
--- Week05/Http_Server/index.js
+++ Week05/Http_Server/index.js
@@ -6,12 +6,35 @@ const app = express();
 
 
 app.get("/sum",function(req,res){
-    
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        answer: a+b
+    })
 })
 
 
 app.get("/multiply",function(req,res){
-    
+    const a = req.query.a;
+    const b = req.query.b;
+
+    res.json({
+        answer: a*b
+    })
 })
+
+
+// dynamic routing
+
+app.get("/sum1/:c/:d",function(req,res){
+    const a = parseInt(req.query.a);
+    const b = parseInt(req.query.b);
+
+    res.json({
+        answer: a+b
+    })
+})
+
 
 app.listen(3000);
