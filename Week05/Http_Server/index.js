d321269f2abcf19176ae35164b025abd0a054890
utkarsh240 <93428291+utkarsh240@users.noreply.github.com>
2025-01-25 17:04:55 +0530

    .

---
+++ Week05/Http_Server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const fs = require("fs");
+
+
+const app = express(); 
+
+
+app.get("/sum",function(req,res){
+    
+})
+
+
+app.get("/multiply",function(req,res){
+    
+})
+
+app.listen(3000);
