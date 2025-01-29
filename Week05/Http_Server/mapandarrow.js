86e1f5c219b0ed7fff783feee4ba27ac57558521
utkarsh240 <93428291+utkarsh240@users.noreply.github.com>
2025-01-29 22:14:36 +0530

    .

---
+++ Week05/Http_Server/mapandarrow.js
@@ -0,0 +1,34 @@
+// till now
+
+
+
+// function sum(a,b){
+//     return a+b;
+// }
+
+// const ans = sum(1,2);
+// console.log(ans);
+
+// from now
+
+// const input= [3,4,5,6,7];
+
+// function transform(i){
+
+//     return i*2;
+// }
+
+// const ans = input.map(transform);
+
+// console.log(ans);
+
+const input= [3,4,5,6,7];
+
+
+
+const ans = input.map(function(i){
+
+    return i*2;
+});
+
+console.log(ans);
