81579e29296089ca6ca48239a882d3e4b5bad6ae
utkarsh240 <93428291+utkarsh240@users.noreply.github.com>
2025-02-10 19:10:54 +0530

    .

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+app.post("/signup", function(req,res){
+
+})
+
+app.post("/signin", function(req,res){
+    
+})
+
+
+app.post("/todo", function(req,res){
+    
+})
+
+app.get("/todos", function(req,res){
+    
+})
+
+
+app.listen(3000);
