ad6eda7157d68b649c26f5aea5b4ad0ce80d3b5b
utkarsh240 <93428291+utkarsh240@users.noreply.github.com>
2025-02-22 23:23:49 +0530

    Update index.js

--- Week07/index.js
+++ Week07/index.js
@@ -7,6 +7,7 @@ const mongoose = require("mongoose");
 const { hash } = require("bcrypt");
 
 
+
 const app = express();
 app.use(express.json());
 
@@ -15,6 +16,9 @@ app.post("/signup", async function(req, res) {
     const password = req.body.password;
     const name = req.body.name;
 
+    let errorThrown = false;
+
+    try{
     const hashedPassword =  await bcrypt.hash(password,5);
     console.log(hashedPassword);
 
@@ -24,7 +28,17 @@ app.post("/signup", async function(req, res) {
         password: hashedPassword, 
         name: name
     });
-    
+
+    throw new error("User already exist");
+
+ }catch(e){
+    console.log("Error while putting error");
+    res.json({
+        message: "User already exist"
+    })
+    errorThrown = true;
+
+ }
     res.json({
         message: "You are signed up"
     })
@@ -36,11 +50,19 @@ app.post("/signin", async function(req, res) {
     const password = req.body.password;
 
     const response = await UserModel.findOne({
-        email: email,
-        password: password,
+        email: email
     });
 
-    if (response) {
+    if(!response){
+        res.json(403).json({
+            message: "User doesnt exist in db"
+        })
+        return
+    }
+
+    const passwordMatch = await bcrypt.compare(password, response.password);
+
+    if (passwordMatch) {
         const token = jwt.sign({
             id: response._id.toString()
         }, JWT_SECRET);
