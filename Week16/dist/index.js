c75dbc60970ac2648ec415454f6ce5e464f98284
Utkarsh <93428291+utkarsh240@users.noreply.github.com>
2025-06-18 19:59:34 +0530

    .

--- Week16/dist/index.js
+++ Week16/dist/index.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log("hi");
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = require("ws");
+const wss = new ws_1.WebSocketServer({ port: 8080 });
+// wss.on('connection', function connection(ws) {
+//   ws.on('error', console.error);
+//   ws.on('message', function message(data) {
+//     console.log('received: %s', data);
+//   });
+//   ws.send('something');
+// });
+wss.on("connection", function (socket) {
+    console.log("user connected");
+    setInterval(() => {
+        socket.send("current price of eth is" + Math.random());
+    }, 500);
+});
--- Week16/src/index.ts
+++ Week16/src/index.ts
@@ -1,1 +1,22 @@
-console.log("hi");
+import { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+// wss.on('connection', function connection(ws) {
+//   ws.on('error', console.error);
+
+//   ws.on('message', function message(data) {
+//     console.log('received: %s', data);
+//   });
+
+//   ws.send('something');
+// });
+
+wss.on("connection", function(socket){
+console.log("user connected")
+setInterval(()=>{
+  socket.send("current price of eth is"+Math.random())
+},500)
+
+
+})
