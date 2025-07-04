f65e1cc242148c957253b559b27e00e6bd630a3c
Utkarsh <93428291+utkarsh240@users.noreply.github.com>
2025-07-04 15:40:08 +0530

    Add basic WebSocket server implementation
    Introduced a simple WebSocket server using the ws library. The server tracks connected users and broadcasts received messages to all clients. Added TypeScript configuration, package files, and build scripts for project setup.

---
+++ week17-2/dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = require("ws");
+const wss = new ws_1.WebSocketServer({ port: 8080 });
+let userCount = 0;
+let allSockets = [];
+wss.on('connection', (socket) => {
+    allSockets.push(socket);
+    userCount = userCount + 1;
+    console.log("user connected" + userCount);
+    socket.on("message", (message) => {
+        console.log("message recieved " + message.toString());
+        for (let i = 0; i < allSockets.length; i++) {
+            const s = allSockets[i];
+            s.send(message.toString() + "message sent from server");
+        }
+    });
+});
