a377b62494258cf102635b828c0825cbf40d7051
Utkarsh <93428291+utkarsh240@users.noreply.github.com>
2025-07-06 21:45:21 +0530

    Refactor socket message broadcast and handle disconnects
    Replaces for-loop with forEach for broadcasting messages to all sockets and updates the message format. Adds a disconnect event handler to remove sockets from the allSockets array when a client disconnects.

--- week17-2/src/index.ts
+++ week17-2/src/index.ts
@@ -6,15 +6,19 @@ let userCount =0;
 let allSockets: WebSocket[] =[];
 wss.on('connection',(socket)=>{
     allSockets.push(socket);
-    userCount = userCount+1;
+    
     console.log("user connected"+ userCount);
 
     socket.on("message",(message)=>{
         console.log("message recieved " + message.toString())
-        for(let i =0;i<allSockets.length; i++){
-            const s = allSockets[i];
-            s.send(message.toString()+ "message sent from server")
-        }
+        allSockets.forEach(s=>{
+            s.send(message.toString()+ "sent from server")
+        })
         
     })
+
+    socket.on("disconnect",()=>{
+        allSockets = allSockets.filter(x => x!= socket);
+    })
 })
+
